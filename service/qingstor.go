8a0e14c727e57fb7a4954e75499e5cc311509e8d
qiaoqiaolaile <2457254782@qq.com>
2018-06-26 15:04:24 +0800

    Add files via upload

--- service/qingstor.go
+++ service/qingstor.go
@@ -14,95 +14,188 @@
 // | limitations under the License.
 // +-------------------------------------------------------------------------
 
+
+
 // Package service provides QingStor Service API (API Version 2016-01-06)
 package service
 
 import (
-	"net/http"
+    "net/http"
 
-	"github.com/yunify/qingstor-sdk-go/config"
-	"github.com/yunify/qingstor-sdk-go/request"
-	"github.com/yunify/qingstor-sdk-go/request/data"
+    "github.com/yunify/qingstor-sdk-go/config"
+    "github.com/yunify/qingstor-sdk-go/request"
+    "github.com/yunify/qingstor-sdk-go/request/data"
 )
 
 var _ http.Header
 
 // Service QingStor provides low-cost and reliable online storage service with unlimited storage space, high read and write performance, high reliability and data safety, fine-grained access control, and easy to use API.
 type Service struct {
-	Config *config.Config
+    Config *config.Config
 }
 
 // Init initializes a new service.
 func Init(c *config.Config) (*Service, error) {
-	return &Service{Config: c}, nil
+    return &Service{Config: c}, nil
 }
 
-// ListBuckets does Retrieve the bucket list.
-// Documentation URL: https://docs.qingcloud.com/qingstor/api/service/get.html
-func (s *Service) ListBuckets(input *ListBucketsInput) (*ListBucketsOutput, error) {
-	r, x, err := s.ListBucketsRequest(input)
 
-	if err != nil {
-		return x, err
-	}
+    
+    
+    
 
-	err = r.Send()
-	if err != nil {
-		return nil, err
-	}
+    
+    
+    
 
-	requestID := r.HTTPResponse.Header.Get(http.CanonicalHeaderKey("X-QS-Request-ID"))
-	x.RequestID = &requestID
+    
+    
 
-	return x, err
-}
+    
+    
+    
+    
+    
+    
 
-// ListBucketsRequest creates request and output object of ListBuckets.
-func (s *Service) ListBucketsRequest(input *ListBucketsInput) (*request.Request, *ListBucketsOutput, error) {
+    // ListBuckets does Retrieve the bucket list.
+        // Documentation URL: https://docs.qingcloud.com/qingstor/api/service/get.html
+    func (s *Service) ListBuckets(input *ListBucketsInput) (*ListBucketsOutput, error) {
+    r, x, err := s.ListBucketsRequest(input)
+        
+        if err != nil {
+            return x, err
+        }
 
-	if input == nil {
-		input = &ListBucketsInput{}
-	}
+        err = r.Send()
+        if err != nil {
+            return nil, err
+        }
 
-	o := &data.Operation{
-		Config:        s.Config,
-		APIName:       "Get Service",
-		RequestMethod: "GET",
-		RequestURI:    "/",
-		StatusCodes: []int{
-			200, // OK
-		},
-	}
+        requestID := r.HTTPResponse.Header.Get(http.CanonicalHeaderKey("X-QS-Request-ID"))
+        x.RequestID = &requestID
 
-	x := &ListBucketsOutput{}
-	r, err := request.New(o, input, x)
-	if err != nil {
-		return nil, nil, err
-	}
+        return x, err
+    }
 
-	return r, x, nil
-}
+    // ListBucketsRequest creates request and output object of ListBuckets.
+        func (s *Service) ListBucketsRequest(input *ListBucketsInput) (*request.Request, *ListBucketsOutput, error) {
+    
+            if input == nil {
+                input = &ListBucketsInput{}
+            }
+        
+        
+        
+        
+        
+        
 
-// ListBucketsInput presents input for ListBuckets.
-type ListBucketsInput struct {
-	// Limits results to buckets that in the location
-	Location *string `json:"Location,omitempty" name:"Location" location:"headers"`
-}
+        o := &data.Operation{
+            Config:        s.Config,
+            APIName:       "Get Service",
+            RequestMethod: "GET",
+            RequestURI:    "/",
+            StatusCodes: []int{
+                200, // OK
+                    },
+        }
 
-// Validate validates the input for ListBuckets.
-func (v *ListBucketsInput) Validate() error {
+        x := &ListBucketsOutput{}
+        r, err := request.New(o, input, x)
+        if err != nil {
+            return nil, nil, err
+        }
 
-	return nil
-}
+        return r, x, nil
+    }
 
-// ListBucketsOutput presents output for ListBuckets.
-type ListBucketsOutput struct {
-	StatusCode *int `location:"statusCode"`
+    
+        // ListBucketsInput presents input for ListBuckets.
+        type ListBucketsInput struct {
+                // Limits results to buckets that in the location
+            Location *string `json:"Location,omitempty" name:"Location" location:"headers"` 
+    
 
-	RequestID *string `location:"requestID"`
+            
+        }
 
-	// Buckets information
-	Buckets []*BucketType `json:"buckets,omitempty" name:"buckets" location:"elements"`
-	// Bucket count
-	Count *int `json:"count,omitempty" name:"count" location:"elements"`
-}
+        // Validate validates the input for ListBuckets.
+        func (v *ListBucketsInput) Validate() error {
+            
+    
+    
+
+    
+
+            
+    
+    
+
+    
+        
+            
+            
+                
+                
+                
+            
+
+            
+
+            
+        
+    
+
+            
+    
+    
+
+    
+
+
+            return nil
+        }
+    
+
+    // ListBucketsOutput presents output for ListBuckets.
+    type ListBucketsOutput struct {
+        StatusCode *int `location:"statusCode"`
+
+        RequestID *string `location:"requestID"`
+        
+
+            
+                
+                // Buckets information
+            Buckets []*BucketType `json:"buckets,omitempty" name:"buckets" location:"elements"` 
+    // Bucket count
+            Count *int `json:"count,omitempty" name:"count" location:"elements"` 
+    
+
+            
+
+            
+        
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
