ea969d40a22fa7a6a6f81ea9cc0a64ef8a92b635
Ubique0305 <1071763478@qq.com>
2019-07-16 14:22:53 +0800

    fix nil check to make addmeta work normally
    Signed-off-by: Ubique0305 <1071763478@qq.com>

--- service/check_metadata.go
+++ service/check_metadata.go
@@ -8,63 +8,59 @@ func IsMetaDataValid(XQSMetaData *map[string]string) error {
 	XQSMetaDataIsValid := true
 	wrongKey := ""
 	wrongValue := ""
-	if *XQSMetaData == nil {
-		XQSMetaDataIsValid = false
-	}
-	if *XQSMetaData!= nil {
-		metadataValuelength := 0
-		metadataKeylength := 0
+	
+	metadataValuelength := 0
+	metadataKeylength := 0
 
-		for k,v := range *XQSMetaData {
-			metadataKeylength += len(k)
-			metadataValuelength += len(v)
-			startstr:=strings.Split(k,"-")
-			if len(startstr) < 4 {
-				wrongKey = k
-				wrongValue = v
-				XQSMetaDataIsValid = false
-				break
-			}
-			if startstr[0] != "x"||startstr[1] != "qs"||startstr[2] != "meta"||startstr[3] == ""{
-				wrongKey = k
-				wrongValue = v
-				XQSMetaDataIsValid = false
-				break
-			}
+	for k,v := range *XQSMetaData {
+		metadataKeylength += len(k)
+		metadataValuelength += len(v)
+		startstr:=strings.Split(k,"-")
+		if len(startstr) < 4 {
+			wrongKey = k
+			wrongValue = v
+			XQSMetaDataIsValid = false
+			break
+		}
+		if startstr[0] != "x"||startstr[1] != "qs"||startstr[2] != "meta"||startstr[3] == ""{
+			wrongKey = k
+			wrongValue = v
+			XQSMetaDataIsValid = false
+			break
+		}
 
-			for i := 0; i < len(k); i++ {
-				ch := k[i]
-				if!(ch >= 65&&ch <= 90||ch >= 97&&ch <= 122||ch <= 57&&ch >= 48||ch == 45||ch == 46){
-					wrongKey = k
-					wrongValue = v
-					XQSMetaDataIsValid = false
-					break
-				}
-			}
-			for i := 0; i < len(v); i++ {
-				ch := v[i]
-				if(ch < 32||ch > 126){
-					wrongKey = k
-					wrongValue = v
-					XQSMetaDataIsValid = false
-					break
-				}
-			}
-
-			if metadataKeylength > 512{
-				wrongKey = k
-				wrongValue = v
-				XQSMetaDataIsValid = false
-				break
-			}
-			if metadataValuelength > 2048{
+		for i := 0; i < len(k); i++ {
+			ch := k[i]
+			if!(ch >= 65&&ch <= 90||ch >= 97&&ch <= 122||ch <= 57&&ch >= 48||ch == 45||ch == 46){
 				wrongKey = k
 				wrongValue = v
 				XQSMetaDataIsValid = false
 				break
 			}
 		}
+		for i := 0; i < len(v); i++ {
+			ch := v[i]
+			if(ch < 32||ch > 126){
+				wrongKey = k
+				wrongValue = v
+				XQSMetaDataIsValid = false
+				break
+			}
+		}
+		if metadataKeylength > 512{
+			wrongKey = k
+			wrongValue = v
+			XQSMetaDataIsValid = false
+			break
+		}
+		if metadataValuelength > 2048{
+			wrongKey = k
+			wrongValue = v
+			XQSMetaDataIsValid = false
+			break
+		}
 	}
+
 	if !XQSMetaDataIsValid {
 		return errors.ParameterValueNotAllowedError{
 			ParameterName: "XQSMetaData",
